7522f925e3c9d27282b944c7719bef65d1f540b4
HuntAs01 <ashley.hunt@lexisnexisrisk.com>
2022-09-15 11:26:39 +0100

    Fetching from API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,24 @@
-    
+import React, { useState } from "react";
+
 export default function Home() {
-  return <h2>Home</h2>;
+  const [data, setData] = useState("");
+
+  if (data.length === 0) {
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }
+
+  return (
+    <div>
+      {data && (
+        <h2>
+          Height of {data.name} is {data.height}
+        </h2>
+      )}
+    </div>
+  );
 }
