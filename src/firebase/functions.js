6da4e3b20b7f6e102dcc36b5f976997ff791e863
HuntAs01 <ashley.hunt@lexisnexisrisk.com>
2022-09-19 23:04:20 +0100

    Improved database functions

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -6,37 +6,61 @@ import {
   setDoc,
   getDocs,
   query,
+  where,
 } from "firebase/firestore";
 
 // Find documentation for firebase functions here: https://firebase.google.com/docs/firestore/quickstart
 
 // https://firebase.google.com/docs/firestore/manage-data/add-data
-export const addUser = async (userID, data) => {
+export const addUser = async (data) => {
   try {
-    await setDoc(doc(db, "users", userID), data);
+    if (await userExists(data)) {
+      console.log(
+        `Error: User ${data.firstName} ${data.lastName}  already exists`
+      );
+    } else {
+      await setDoc(doc(db, "users", data.firstName + data.lastName), data);
+      console.log("User created");
+    }
   } catch (err) {
     console.log(err);
   }
 };
 
-export const getData = async () => {
-  const docRef = doc(db, "users", "y5ii2Nz8djpsDHbQQpOT");
+export const userExists = async (data) => {
+  try {
+    const q = query(
+      collection(db, "users"),
+      where("firstName", "==", data.firstName),
+      where("lastName", "==", data.lastName)
+    );
+    const querySnapshot = await getDocs(q);
+    return !querySnapshot.empty;
+  } catch (error) {
+    console.log(error);
+    return true;
+  }
+};
+
+export const getData = async (userID) => {
+  const docRef = doc(db, "users", userID);
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
     console.log("Document data:", docSnap.data());
   } else {
-    // doc.data() will be undefined in this case
     console.log("No such document!");
   }
 };
 
 export const getAll = async () => {
   const q = query(collection(db, "users"));
+  const results = await getDocs(q);
 
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
+  let users = [];
+  results.forEach((user) => {
+    let data =  {...user.data(), id: user.id}
+    users.push(data);
   });
+  return users;
 };
