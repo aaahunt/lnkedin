15d4ca0c8f17a84d092f7329bb7a32583804d76e
HuntAs01 <ashley.hunt@lexisnexisrisk.com>
2022-09-19 13:47:20 +0100

    Organised firebase functions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { db } from "../config";
-import {collection, addDoc} from 'firebase/firestore'
+import { getData, getAll, addData } from "../firebase/functions";
 
 export default function Home() {
   const [data, setData] = useState([]);
 
 
-  const handleSubmit = async () => {
-    try {
-      await addDoc(collection(db, 'users'), {
-        firstName: "Testerman",
-        lastName: "description",
-      })
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-
-    handleSubmit();
-
-    fetch("http://localhost:4000/users")
-      .then((response) => response.json())
-      .then((jsonData) => {
-        setData(jsonData);
-      });
-  }, []);
-
   return (
     <div>
+      <button onClick={() => getData()}>Get Data</button>
+      <button onClick={() => getAll()}>Get All</button>
+      
       {data.map((item) => (
         <div key={item.PersonID}>
-          <h1>Name: {item.FirstName} {item.LastName}</h1>
+          <h1>
+            Name: {item.FirstName} {item.LastName}
+          </h1>
           <h3>{item.City}</h3>
         </div>
       ))}
