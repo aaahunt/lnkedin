75b109c3acb83cb612f719a0235749508ee4298c
HuntAs01 <ashley.hunt@lexisnexisrisk.com>
2022-09-19 16:36:45 +0100

    Added firebaseConfig without API Key

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: "yellow-94486.firebaseapp.com",
+    projectId: "yellow-94486",
+    storageBucket: "yellow-94486.appspot.com",
+    messagingSenderId: "16377800952",
+    appId: "1:16377800952:web:6f8e574609b04e1e73330d",
+    measurementId: "G-4GN594K8EV"
+  };
