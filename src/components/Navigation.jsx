fc5091f5cb81bd1f9b71a160dde21ce5bb375dc0
HuntAs01 <ashley.hunt@lexisnexisrisk.com>
2022-09-20 13:25:19 +0100

    Fix assigned but not used statement

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,9 +13,9 @@ export default function Navigation() {
         <li>
           <Link to="/users">Users</Link>
         </li>
-        <li>
-          <Link to="/profile">My Profile</Link>
-        </li>
+          <li>
+            <Link to="/profile">My Profile</Link>
+          </li>
       </ul>
       <div className="rightNav">
         <input type="text" name="search" id="search" />
