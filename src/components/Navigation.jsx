d494ee19d62b1e2cd80a6be23782b00545b025c3
marro1212 <olivia.marr@cirium.com>
2022-09-20 13:56:34 +0100

    Login fixed

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
-  let showProfilePage = /users|profile/.test(useLocation().pathname);
 
   return (
     <nav className="navbar background">
