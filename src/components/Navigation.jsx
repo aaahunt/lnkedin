2b41a3f9b7e8e8b73e092d85e2fac3022f3110ae
HuntAs01 <ashley.hunt@lexisnexisrisk.com>
2022-09-20 22:34:33 +0100

    Removed about page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -15,9 +15,6 @@ export default function Navigation() {
         <li>
           <Link to="/profile">My Profile</Link>
         </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
       </ul>
       <div className="rightNav">
         <input type="text" name="search" id="search" />
