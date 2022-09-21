471315c84cf72c367a751314503048a907165976
HuntAs01 <ashley.hunt@lexisnexisrisk.com>
2022-09-21 13:01:05 +0100

    remove unused import

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
-import { getAll } from "../firebase/functions";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import * as React from 'react';
-import { Button, CardActions } from '@mui/material';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { Button, CardActions } from "@mui/material";
 import Search from "../components/Search";
 
 export default function Users(props) {
-
   const [data, setData] = useState([]);
 
- 
   return (
     <div>
       <h1>{props.db}</h1>
@@ -20,26 +17,30 @@ export default function Users(props) {
       {data.map((item) => (
         <div key={item.id}>
           <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-          <Typography variant="h5" component="div" align="left" >
-          {item.firstName} {item.lastName}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} align="left">
-          Scheme: {item.role}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} align="left">
-          Office: {item.officeLocation}
-        </Typography>
-        <CardActions>
-        <Button href={`/user?id=${item.id}`} size="small" color="primary" style={{marginRight:"auto"}} > 
-          View Profile
-        </Button>
-        </CardActions>
-          </CardContent>
+            <CardContent>
+              <Typography variant="h5" component="div" align="left">
+                {item.firstName} {item.lastName}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} align="left">
+                Scheme: {item.role}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} align="left">
+                Office: {item.officeLocation}
+              </Typography>
+              <CardActions>
+                <Button
+                  href={`/user?id=${item.id}`}
+                  size="small"
+                  color="primary"
+                  style={{ marginRight: "auto" }}
+                >
+                  View Profile
+                </Button>
+              </CardActions>
+            </CardContent>
           </Card>
         </div>
       ))}
     </div>
   );
 }
-
